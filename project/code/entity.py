d792e984b024215a8b4d60054a0bc54d5f6e60b7
AlexandruMorar00 <alexandru.morar00@e-uvt.ro>
2022-05-30 09:28:10 +0300

    initial commit

---
+++ project/code/entity.py
@@ -0,0 +1,50 @@
+import pygame
+from math import sin
+
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        # get direction
+        # based on x,y coordinates that can modify by player's speed (pixels/frame)
+        self.direction = pygame.math.Vector2()
+
+    def move(self, speed):
+        # have to normalize the direction ( change the length of vector to 1 )
+        # this is for constant move on diagonals
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        # direction speed
+
+        self.hitbox.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.hitbox.y += self.direction.y * speed
+        self.collision('vertical')
+        self.rect.center = self.hitbox.center
+       # self.rect.center += self.direction * speed --> method splitted apart above
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0:  # moving right
+                        self.hitbox.right = sprite.hitbox.left
+                    if self.direction.x < 0:  # moving left
+                        self.hitbox.left = sprite.hitbox.right
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0:  # moving down
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0:  # moving up
+                        self.hitbox.top = sprite.hitbox.bottom
+
+    # flicker
+    def wave_value(self):
+        value = sin(pygame.time.get_ticks())
+        if value >= 0:
+            return 255
+        else:
+            return 0
